12b14a0cf3274ddec9a5e4706e426d3fdcf19ef7
luecy1 <kowaheno@gmail.com>
2017-12-24 23:10:00 +0900

    バグ修正

--- app/src/main/java/com/example/luecy1/nhkbangumi/util/CommonUtils.java
+++ app/src/main/java/com/example/luecy1/nhkbangumi/util/CommonUtils.java
@@ -3,6 +3,7 @@ package com.example.luecy1.nhkbangumi.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by you on 2017/12/23.
@@ -10,14 +11,14 @@ import java.util.Date;
 
 public class CommonUtils {
 
-    private static final SimpleDateFormat NHK_DATE_FORMAT = new SimpleDateFormat("YYYY-MM-DD HH:mm:ssZ") ;
-
     // no instance
     private CommonUtils() {
     }
 
-    synchronized public static Date string2Date(String dateString) throws ParseException {
+    public static Date string2Date(String dateString) throws ParseException {
+        final SimpleDateFormat nhkDateFormat = new SimpleDateFormat("yyyy-MM-DD HH:mm:ssZ", Locale.JAPAN);
+
         dateString = dateString.replace("T"," ");
-        return NHK_DATE_FORMAT.parse(dateString);
+        return nhkDateFormat.parse(dateString);
     }
 }
